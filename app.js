86373e2a23e8e78f8b4d59a29a22471cbcc379e5
CodingSpotYT <syamsireeshaj@gmail.com>
2025-01-22 21:15:37 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('.read-more').forEach((button) => {
+    button.addEventListener('click', () => {
+      const card = button.closest('.project-card');
+      const fullDescription = card.querySelector('.full-description');
+      const description = card.querySelector('.description');
+  
+      // Toggle expanded state
+      card.classList.toggle('expanded');
+      if (card.classList.contains('expanded')) {
+        fullDescription.style.display = 'block'; // Show full description
+        description.style.display = 'none'; // Hide the truncated description
+        button.textContent = 'Read Less'; // Change button text
+      } else {
+        fullDescription.style.display = 'none'; // Hide full description
+        description.style.display = 'block'; // Show truncated description
+        button.textContent = 'Read More'; // Change button text
+      }
+    });
+  });
+  
